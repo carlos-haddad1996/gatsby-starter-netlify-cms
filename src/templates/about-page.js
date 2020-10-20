2b2f757abef1d56df2c5d238e8bf5218d69706e5
Carlor Haddad <carlos.haddad@partnerhero.com>
2020-10-20 14:41:32 -0600

    Testing variables

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {    
     const PageContent = contentComponent || Content;
 
     console.log('title', title);
